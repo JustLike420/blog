c5d2376cbdc41ae75cd683153097e84ec9dab671
JustLike420 <justlikevova@gmail.com>
2020-11-10 18:54:58 +0300

    Modify admin

--- siteblog/admin.py
+++ siteblog/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Post, Tag, Category
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}  # автоматическое составление url из название
-    save_as = True
+    save_as = True  # после добавления, текст остается
+    save_on_top = True  # кнопка сохранения сверху
+    list_display = ('id', 'title', 'slug', 'category', 'created_at', 'get_photo')  # какие поля показываются в admin
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)  # поиск по названию темы
+    list_filter = ('category',)  # фильт по категориям
+    read_only_fields = ('views',)
+
+    # вывод фото поста в admin
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="50">')
+        return '-'
+
+    get_photo.short_description = 'Фото'
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
--- siteblog/models.py
+++ siteblog/models.py
@@ -24,6 +24,8 @@ class Category(models.Model):
         return self.title
 
     class Meta:
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
         ordering = ['title']  # сортирока по имяни в алф порядке
 
 class Tag(models.Model):
@@ -34,22 +36,26 @@ class Tag(models.Model):
         return self.title
 
     class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
         ordering = ['title']  # сортирока по имяни в алф порядке
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, verbose_name='Название')
     slug = models.SlugField(max_length=255, verbose_name='url', unique=True)
     author = models.CharField(max_length=100)
     content = CKEditor5Field('Text', config_name='extends')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')
     photo = models.ImageField(blank=True, default='photo.png')
     views = models.IntegerField(default=0, verbose_name='Кол-во просмотров')
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts', verbose_name='Категория')
     tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
 
     def __str__(self):
         return self.title
 
     class Meta:
+        verbose_name = 'Статью'
+        verbose_name_plural = 'Статьи'
         ordering = ['-created_at']  # сортирока по дате в обратном порядке свежие выше
